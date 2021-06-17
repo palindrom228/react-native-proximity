fa495bfe0d1df4da23cbc50914d6f9dc12cac162
Tamir Shoboev <tamirshoboev@Mac-mini-Tamir.local>
2021-06-17 23:02:40 +0300

    lastEdit

--- android/src/main/java/com/RNProximity/RNProximityPackage.java
+++ android/src/main/java/com/RNProximity/RNProximityPackage.java
@@ -15,11 +15,6 @@ public class RNProximityPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNProximityModule(reactContext));
     }
-
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
